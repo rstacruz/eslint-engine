fa1062c6a8974eccf2ccbb62ae4ca27de8ddbc8a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-12 10:57:35 +0800

    Print 'ok' when passing

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = function tapeEslint (options) {
   return function (t) {
     eslint(options, function (err, res) {
       if (err) return t.fail(err)
-      errorify(t, res)
+      var count = errorify(t, res)
+      if (count === 0) t.pass('passed')
       t.end()
     })
   }
@@ -15,9 +16,11 @@ module.exports = function tapeEslint (options) {
  */
 
 function errorify (t, res) {
+  var messages = 0
   res.results.forEach(function (result) {
     if (result.errorCount || result.warningCount) {
       result.messages.forEach(function (msg) {
+        messages += 1
         t.fail('' +
           result.filePath.replace(process.cwd(), '') +
           ':' + msg.line + ':' + msg.column + ': ' +
@@ -25,4 +28,5 @@ function errorify (t, res) {
       })
     }
   })
+  return messages
 }
