32acf47a948e52a3504ce5c14bb1f4880a08a1f9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-13 18:59:37 +0800

    Update message in the browser

--- browser.js
+++ browser.js
@@ -1,6 +1,6 @@
-module.exports = function tapeStandard (options) {
+module.exports = function tapeEslint (options) {
   return function (t) {
-    t.pass('tape-standard not supported on this platform')
+    t.pass('tape-eslint not supported on this platform')
     t.end()
   }
 }
